b2542e2f5be6ad4fee46a6ec431127abfd2233fe
Илья Окороков <ilyaokorokov03@gmail.com>
2024-05-24 09:33:15 +0300

    hw6.1_remastered

--- lesson_6.1/test_auth_form.py
+++ lesson_6.1/test_auth_form.py
@@ -12,40 +12,36 @@ def test_auth_form():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-    first_name = driver.find_element(By.CSS_SELECTOR, 'input[name="first-name"]')
-    first_name.send_keys("Иван")
-
-    last_name = driver.find_element(By.CSS_SELECTOR, 'input[name="last-name"]')
-    last_name.send_keys("Петров")
-
-    address = driver.find_element(By.CSS_SELECTOR, 'input[name="address"]')
-    address.send_keys("Ленина, 55-3")
-
-    email = driver.find_element(By.CSS_SELECTOR, 'input[name="e-mail"]')
-    email.send_keys("test@skypro.com")
-
-    phone_number = driver.find_element(By.CSS_SELECTOR, 'input[name="phone"]')
-    phone_number.send_keys("+7985899998787")
-
-    zip_code = driver.find_element(By.CSS_SELECTOR, 'input[name="zip-code"]')
-    zip_code.clear()
-
-    city = driver.find_element(By.CSS_SELECTOR, 'input[name="city"]')
-    city.send_keys("Москва")
-
-    country = driver.find_element(By.CSS_SELECTOR, 'input[name="country"]')
-    country.send_keys("Россия")
-
-    job_position = driver.find_element(By.CSS_SELECTOR, 'input[name="job-position"]')
-    job_position.send_keys("QA")
-
-    company = driver.find_element(By.CSS_SELECTOR, 'input[name="company"]')
-    company.send_keys("SkyPro")
-
-    submit_button = driver.find_element(
-        By.CSS_SELECTOR, 'button.btn-outline-primary[type="submit"]'
+    driver.find_element(By.CSS_SELECTOR, 'input[name="first-name"]').send_keys("Иван")
+    driver.find_element(By.CSS_SELECTOR, 'input[name="last-name"]').send_keys("Петров")
+    driver.find_element(By.CSS_SELECTOR, 'input[name="address"]').send_keys(
+        "Ленина, 55-3"
     )
-    submit_button.click()
+    driver.find_element(By.CSS_SELECTOR, 'input[name="e-mail"]').send_keys(
+        "test@skypro.com"
+    )
+    driver.find_element(By.CSS_SELECTOR, 'input[name="phone"]').send_keys(
+        "+7985899998787"
+    )
+    driver.find_element(By.CSS_SELECTOR, 'input[name="zip-code"]').clear()
+    driver.find_element(By.CSS_SELECTOR, 'input[name="city"]').send_keys("Москва")
+    driver.find_element(By.CSS_SELECTOR, 'input[name="country"]').send_keys("Россия")
+    driver.find_element(By.CSS_SELECTOR, 'input[name="job-position"]').send_keys("QA")
+    driver.find_element(By.CSS_SELECTOR, 'input[name="company"]').send_keys("SkyPro")
+    driver.find_element(
+        By.CSS_SELECTOR, 'button.btn-outline-primary[type="submit"]'
+    ).click()
+
+    first_name = driver.find_element(By.CSS_SELECTOR, "#first-name")
+    last_name = driver.find_element(By.CSS_SELECTOR, "#last-name")
+    address = driver.find_element(By.CSS_SELECTOR, "#address")
+    email = driver.find_element(By.CSS_SELECTOR, "#e-mail")
+    phone_number = driver.find_element(By.CSS_SELECTOR, "#phone")
+    zip_code = driver.find_element(By.CSS_SELECTOR, "#zip-code")
+    city = driver.find_element(By.CSS_SELECTOR, "#city")
+    country = driver.find_element(By.CSS_SELECTOR, "#country")
+    job_position = driver.find_element(By.CSS_SELECTOR, "#job-position")
+    company = driver.find_element(By.CSS_SELECTOR, "#company")
 
     success_class = "alert-success"
     danger_class = "alert-danger"
@@ -75,7 +71,5 @@ def test_auth_form():
     )
     danger_element = zip_code.get_attribute("class")
     assert danger_class in danger_element
-
-    print(driver.find_element(By.CSS_SELECTOR, "p.bg-success").text)
 
     driver.quit()
